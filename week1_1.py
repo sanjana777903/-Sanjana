aadd4b2c647c24016a0cf45d5cce6bc866ab9b0a
sanjana777903 <syan0777777@gmail.com>
2025-03-27 02:52:02 -0700

    Add files via upload

---
+++ week1_1.py
@@ -0,0 +1,13 @@
+List=[]
+print("Blank List:")
+print(List)
+List=[10,20,14]
+print("\nList of number:")
+print(List)
+List=["mon","tue","wed"]
+print("\nList Items:")
+print(List[0])
+print(List[2])
+List=[['jan','feb',],['march']]
+print("\nmulti-dimensional List:")
+print(List)
